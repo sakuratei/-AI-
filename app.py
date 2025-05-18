fafc12913de4d8dfac4330e566d68e8dcd997342
sakuratei <drdeath4242@gmail.com>
2025-05-18 17:52:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,9 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=reply_text)
     )
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
